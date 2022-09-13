8e36799297fa1ff8ff86ad3b4fcbfbce6401caab
Stav <stavush93@gmail.com>
2022-09-13 16:07:48 +0300

    create the redux store and provide it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider>
+     <App />
+  </Provider>
+ 
+);
