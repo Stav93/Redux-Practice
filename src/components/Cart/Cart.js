246f7a32ba5fafc2d9b381e1613e3b2e5417f769
Stav <stavush93@gmail.com>
2022-09-18 19:52:42 +0300

    fixed bugs

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+import { useSelector } from "react-redux";
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((cartItem) => 
+          <CartItem
+            key={cartItem.id}
+            item={{
+              title: cartItem.name,
+              quantity: cartItem.quantity,
+              total: cartItem.totalPrice,
+              price: cartItem.price,
+            }}
+          />
+        )}
       </ul>
     </Card>
   );
