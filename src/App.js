dd6b167ea03a4c73722563242fdeac6daf892c93
Stav <stavush93@gmail.com>
2022-10-05 17:29:29 +0300

    sending http request - updating the card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useSelector } from 'react-redux'
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartisVisible)
+  const showCart = useSelector((state) => state.ui.cartisVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-redux-http-da244-default-rtdb.firebaseio.com/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
