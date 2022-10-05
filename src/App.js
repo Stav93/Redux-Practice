40ea3d42f1825971ad288c4123ab25b61ddf7a9a
Stav <stavush93@gmail.com>
2022-10-05 17:51:07 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { uiSliceActions } from "./store/ui-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartisVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch(
-      "https://react-redux-http-da244-default-rtdb.firebaseio.com/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
-  }, [cart]);
+    const sendingCartData = async () => {
+      dispatch(uiSliceActions.showNotification({ status: "pending", title: "Sending...", message: "Sending Cart Data"}))
+      const response = await fetch(
+        "https://react-redux-http-da244-default-rtdb.firebaseio.com/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error("Sending Data Failed")
+      }
+    }
+
+    // const responseData = await response.json();
+    dispatch(uiSliceActions.showNotification({ status: "success", title: "Success!", message: "Sent Cart Data Successfully!" }))
+    
+    sendingCartData().catch(error => {
+      dispatch(uiSliceActions.showNotification({ status: "error", title: "Error!", message: "Sending Cart Data Failed!"}))
+   })
+  }, [cart, dispatch]);
 
   return (
     <Layout>
