8b9180993915dd0b8f2bec95243d518b5ce185ca
Stav <stavush93@gmail.com>
2022-10-05 17:50:38 +0300

    dispatching diffrend object depends on the current status

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartisVisible: false },
+  initialState: { cartisVisible: false, notification: null },
   reducers: {
     toggle(state) {
-      state.cartisVisible = !state.cartisVisible
-    }
-  }
-})
+      state.cartisVisible = !state.cartisVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
 export const uiSliceActions = uiSlice.actions;
 
