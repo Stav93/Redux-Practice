7d5ac314f186a7e68b0e62fba1c65f88df3171f8
Stav <stavush93@gmail.com>
2022-10-08 19:23:48 +0300

    not sending changed state

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -31,8 +31,6 @@ export const fetchCartData = () => {
         })
       );
     }
-
-
   };
 };
 
@@ -49,7 +47,13 @@ export const sendingCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://react-redux-http-da244-default-rtdb.firebaseio.com/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
 
       if (!response.ok) {
