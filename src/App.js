441482f5ffb3e80436fde5d8c73c674255e8d7ea
Stav <stavush93@gmail.com>
2022-10-10 16:54:32 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route } from "react-router-dom"
-import Welcome from "./Welcome"
-import Products from "./Products"
+import { Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import Products from "./Products";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome/>
-      </Route>
-      <Route path="/products">
-        <Products/>
-      </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
     </div>
   );
 }
