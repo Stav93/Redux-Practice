7d7fd6dc258f470248ff02f1bc0d7f14b145f428
Stav <stavush93@gmail.com>
2022-10-10 16:13:06 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import store from './store/index'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-     <App />
-  </Provider>
-);
+root.render(<App />);
