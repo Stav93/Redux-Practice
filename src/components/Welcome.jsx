aeb0f288df11b0b2e3a47c6494ddf99be6c2034e
Stav <stavush93@gmail.com>
2022-10-15 19:12:55 +0300

    adding nested routes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,13 @@
-
+import { Route } from "react-router-dom"
 
 function Welcome() {
   return (
-    <h1>The Welcome Page</h1>
+    <>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user!</p>
+      </Route>
+    </>
   )
 }
 
