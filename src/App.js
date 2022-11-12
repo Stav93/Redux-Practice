c7c80085d22942325bf316ff7469921aed969ac6
Stav <stavush93@gmail.com>
2022-11-12 21:17:51 +0200

    adding the routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components//Products";
-import ProductDetail from "./components/ProductDetail";
-import MainHeader from "./components/MainHeader";
+import { Route, Switch } from 'react-router-dom'
+import AllQuotes from "./pages/AllQuotes"
+import QuoteDetail from "./pages/QuoteDetail"
+import NewQuote from "./pages/NewQuote"
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome"/>
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+          <AllQuotes/>
+      </Route>
+      <Route path="/quotes/:qouteId">
+          <QuoteDetail/>
+      </Route>
+      <Route path="/new-qoute">
+          <NewQuote/>
+      </Route>
+    </Switch>
   );
 }
 
